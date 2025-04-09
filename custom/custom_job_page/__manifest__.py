0099ce782063683814ed2bc415dd8a2b14ad68b4
Preksha Patil <pp@4fo.de>
2025-04-09 18:10:06 +0530

    Add custom job page module

---
+++ custom/custom_job_page/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Job Page',
+    'version': '1.0',
+    'summary': 'Custom job listing page on website',
+    'category': 'Website',
+    'depends': ['website', 'hr'],
+    'data': [
+        'views/job_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
