9d2c88358c1789dad04138bbe75733a1995d4b7e
Preksha Patil <pp@4fo.de>
2025-04-09 13:11:43 +0530

    Updated the custom module

---
+++ custom/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Job Page',
+    'version': '1.0',
+    'summary': 'Custom job page for website',
+    'category': 'Website',
+    'depends': ['website', 'hr'],
+    'data': [
+        'views/job_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
