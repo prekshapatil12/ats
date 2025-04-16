2a9510ef4a588cef13babf4422b07d0381e38161
Preksha Patil <patilpreksha1211@gmail.com>
2025-04-16 12:54:30 +0530

    ats

---
+++ custom_job/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom Jobs',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Job Listing and Application Page',
+    'description': 'Allows recruiters to post jobs and candidates to view them.',
+    'author': 'Your Name',
+    'website': 'https://yourcompany.com',
+    'depends': ['base', 'website'],
+    'data': [
+        'views/job_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
