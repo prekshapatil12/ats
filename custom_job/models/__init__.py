b900fad91dd5067ddf9621b900af4b716124d51b
Preksha Patil <patilpreksha1211@gmail.com>
2025-04-21 18:28:19 +0530

    Test app

--- custom_job/models/__init__.py
+++ custom_job/models/__init__.py
@@ -1,1 +1,1 @@
-from . import job
+from . import jobs
--- custom_job/models/jobs.py
+++ custom_job/models/jobs.py
@@ -1,13 +1,12 @@
 from odoo import models, fields
-
-
-class JobListing(models.Model):
-    _name = 'custom.job'
-    _description = 'Job Listing'
-
-    name = fields.Char(string='Job Title', required=True)
-    location = fields.Char(string='Location')
-    department = fields.Char(string='Department')
-    description = fields.Text(string='Job Description')
-    requirements = fields.Text(string='Requirements')
-    is_published = fields.Boolean(string='Published', default=True)
+ 
+ class JobListing(models.Model):
+     _name = 'custom.job'
+     _description = 'Job Listing'
+ 
+     name = fields.Char(string='Job Title', required=True)
+     location = fields.Char(string='Location')
+     department = fields.Char(string='Department')
+     description = fields.Text(string='Job Description')
+     requirements = fields.Text(string='Requirements')
+     is_published = fields.Boolean(string='Published', default=True)
