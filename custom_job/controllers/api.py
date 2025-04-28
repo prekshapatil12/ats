d1a5d346e1babe4d4d8f8387a1a63c7f3a015602
Preksha Patil <pp@4fo.de>
2025-04-28 14:20:51 +0530

    Update api.py

--- custom_job/controllers/api.py
+++ custom_job/controllers/api.py
@@ -6,7 +6,7 @@ class JobController(http.Controller):
 
     @http.route('/jobs', type='json', auth='public', methods=['GET'], csrf=False)
     def get_jobs(self):
-        jobs = request.env['jobs.job'].search([])  # Fetch all jobs
+        jobs = request.env['jobs.job'].search([])  
         job_list = []
         for job in jobs:
             job_list.append({
