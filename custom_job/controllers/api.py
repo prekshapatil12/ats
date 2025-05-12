19eaee7c2b7287a2c802fb010bb98025670aec3e
Preksha Patil <pp@4fo.de>
2025-05-12 15:40:13 +0530

    Post Call

--- custom_job/controllers/api.py
+++ custom_job/controllers/api.py
@@ -30,4 +30,43 @@ class JobAPIController(http.Controller):
                 'joining_tentative_date': str(job.joining_tentative_date),
             })
         return {'status': 200, 'jobs': job_list}
+
+@http.route('/api/jobs', type='json', auth='public', methods=['POST'], csrf=False)
+    def create_job_posting(self):
+        try:
+            data = request.jsonrequest  # Get the JSON body
+            new_job = request.env['job.postings'].sudo().create({
+                'job_id': data.get('job_id'),
+                'job_title': data.get('job_title'),
+                'experience': data.get('experience'),
+                'responsibilities': data.get('responsibilities'),
+                'requirement': data.get('requirement'),
+                'skills': data.get('skills'),
+                'status': data.get('status'),
+                'workplace_type': data.get('workplace_type'),
+                'shift': data.get('shift'),
+                'company': data.get('company'),
+                'location': data.get('location'),
+                'salary': data.get('salary'),
+                'posted_date': data.get('posted_date'),
+                'joining_tentative_date': data.get('joining_tentative_date'),
+            })
+            return {
+                'status': 201,
+                'message': 'Job created successfully',
+                'job_id': new_job.id
+            }
+        except Exception as e:
+            return {
+                'status': 500,
+                'error': str(e)
+            }
+
+
+
+
+
+
+
+      
     
