678bf8c8b7fe99004945c2a4ead9d8a8710295e6
Preksha Patil <pp@4fo.de>
2025-05-12 12:58:29 +0530

    Improved POST call

--- custom_job/models/job.py
+++ custom_job/models/job.py
@@ -1,3 +1,29 @@
+# # models/job.py
+# from odoo import models, fields
+
+# class Job(models.Model):
+#     _name = 'job.postings'  # Odoo model name
+#     _table = 'job_postings'  # This is the PostgreSQL table name you're working with
+
+#     job_id = fields.Integer(string='Job ID', required=True)
+#     job_title = fields.Char(string='Job Title')
+#     experience = fields.Char(string='Experience')
+#     responsibilities = fields.Text(string='Responsibilities')
+#     requirement = fields.Text(string='Requirement')
+#     skills = fields.Char(string='Skills')
+#     status = fields.Selection([('t', 'Open'), ('f', 'Closed')], string='Status')
+#     workplace_type = fields.Selection([('Remote', 'Remote'), ('Hybrid', 'Hybrid'), ('On-Site', 'On-Site')], string='Workplace Type')
+#     shift = fields.Selection([('Day', 'Day'), ('Evening', 'Evening')], string='Shift')
+#     company = fields.Char(string='Company')
+#     location = fields.Char(string='Location')
+#     salary = fields.Float(string='Salary')
+#     posted_date = fields.Date(string='Posted Date')
+#     joining_tentative_date = fields.Date(string='Joining Tentative Date')
+
+# models/job.py
+# models/job.py
+from odoo import models, fields
+
 class JobPosting(models.Model):
     _name = 'job.postings'
     _description = 'Job Posting'
