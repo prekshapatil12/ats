943443a760aae72e36ed0910d0e556f0c4c66681
Preksha Patil <pp@4fo.de>
2025-05-12 14:09:16 +0530

    Improved POST call

--- custom_job/controllers/api.py
+++ custom_job/controllers/api.py
@@ -63,4 +63,9 @@ class JobAPIController(http.Controller):
 
         except Exception as e:
             # Return error details if something goes wrong
-            return {'status': 500, 'error': str(e)}
+             _logger.error("Error in job creation: %s", str(e))  # Add this
+             return Response(
+             json.dumps({'status': 500, 'error': str(e)}),
+             content_type='application/json',
+             status=500
+        )
