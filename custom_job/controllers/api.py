27fe58099e3195e35c484f22f6e71a310a6275d6
Preksha Patil <pp@4fo.de>
2025-05-12 17:34:57 +0530

    Post Call

--- custom_job/controllers/api.py
+++ custom_job/controllers/api.py
@@ -31,3 +31,29 @@ class JobAPIController(http.Controller):
             })
         return {'status': 200, 'jobs': job_list}
     
+     @http.route('/api/jobs', type='json', auth='public', methods=['POST'], csrf=False)
+    def get_all_jobs(self):
+        # Fetch all job postings from the job.postings model
+        jobs = request.env['job.postings'].sudo().create([])
+
+        job_list = []
+        for job in jobs:
+            job_list.append({
+                'job_id': job.job_id,
+                'job_title': job.job_title,
+                'experience': job.experience,
+                'responsibilities': job.responsibilities,
+                'requirement': job.requirement,
+                'skills': job.skills,
+                'status': job.status,
+                'workplace_type': job.workplace_type,
+                'shift': job.shift,
+                'company': job.company,
+                'location': job.location,
+                'salary': job.salary,
+                'posted_date': str(job.posted_date),
+                'joining_tentative_date': str(job.joining_tentative_date),
+            })
+        return {'status': 200, 'jobs': job_list}
+    
+    
