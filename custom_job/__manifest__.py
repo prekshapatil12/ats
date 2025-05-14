8b7f0a10272976477dae48dde7d0a3708c6e8a0d
Preksha Patil <pp@4fo.de>
2025-05-14 12:47:06 +0530

    Add View

--- custom_job/__manifest__.py
+++ custom_job/__manifest__.py
@@ -19,6 +19,7 @@ Features:
     'data': [
         # Add your access rights, views, and data files here
         'security/ir.model.access.csv',
+        'views/job_postings_views.xml',
     ],
     'assets': {
         'web.assets_frontend': [
@@ -29,3 +30,4 @@ Features:
     'application': True,
     'auto_install': False,
 }
+
